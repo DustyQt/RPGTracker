bef1f3538e57bb473a87711825215a1f8e81c27e
DustyQt <dustycake1@gmail.com>
2023-08-16 14:09:58 -0500

    adds fits

--- application/src/controller/manager.py
+++ application/src/controller/manager.py
@@ -9,18 +9,22 @@ class manager:
 
     def load_character(self, id):
         character = self.connector.get_item(table_name='character', key={'id': id})
+        character['equipment'] = self.get_character_equipment(character)
         character['basic_stats']= self.calculate_stats(self.get_basic_stats(), character)
         character['classes'] = self.get_classes(character)
         character['class_stats'] = self.get_class_stats(character)
         character['all_pasives_plain'], character['all_pasives_percentage'] = self.get_class_pasives(character)
+        character['info_fits'], character['effect_fits'] = self.get_fits(character)
+        character['all_pasives_plain'], character['all_pasives_percentage'] = self.get_fits_pasives(character, character['all_pasives_plain'], character['all_pasives_percentage'])
+        character['basic_stats']= self.calculate_stats(self.get_basic_stats(), character)
         character['secondary_stats'] = self.calculate_stats(self.get_secondary_stats(), character)
         character['race'] = self.get_race(character)
         character['race_stats'] = self.calculate_stats(self.get_race_stats(character), character)
         for index, value in enumerate(character['classes']):
             character['classes'][index]['precalculated_effect_skills'] = self.pre_calculate_skills(character, value)
         return character
-
-    def get_classes(self, character): 
+ 
+    def get_classes(self, character):
         character_classes = list()
         for char_class in character['classes']:
             character_classes.append(self.connector.get_item(table_name='class', key={'id': char_class}))
@@ -45,19 +49,46 @@ class manager:
         sum_class_pasives_percentage = dict()
         for char_pasive_plain in char_class_pasives_plain:
             for effect in char_pasive_plain['effects']:
-                if effect.get('type') == 'pasive':
+                if effect.get('effect') == 'PASSIVE':
                     stat = effect['stat']
                     formula = effect['formula']
                     dv = effect['default_values']
                     self.sum_dict(sum_class_pasives_plain, stat, self.calculate(character, formula, dv))
         for char_pasive_percentage in char_class_pasives_percentage:
             for effect in char_pasive_percentage['effects']:
-                if effect.get('type') == 'pasive':
+                if effect.get('effect') == 'PASSIVE':
                     stat = effect['stat'] 
                     formula = effect['formula']
                     dv = effect['default_values']
                     self.sum_dict(sum_class_pasives_percentage, stat, self.calculate(character, formula, dv))
         return sum_class_pasives_plain, sum_class_pasives_percentage
+    
+    def get_fits_pasives(self, character, sum_fits_passives_plain, sum_fits_pasives_percentage):
+        fit_passives_plain= list()
+        fit_passives_percentage= list()
+        passive_fits = list()
+        for fit in character['passive_fits']:
+            passive_fits.append(self.connector.get_item(table_name='fit', key={'id': fit}))
+        for fit in passive_fits:
+            if fit['type'] == 'passive_plain':
+                fit_passives_plain.append(fit)
+            if fit['type'] == 'passive_percentage':
+                fit_passives_percentage.append(fit)
+        for fit in fit_passives_plain: 
+            for effect in fit['effects']:
+                if effect.get('effect') == 'PASSIVE':
+                    stat = effect['stat']
+                    formula = effect['formula']
+                    dv = effect['default_values']
+                    self.sum_dict(sum_fits_passives_plain, stat, self.calculate(character, formula, dv))
+        for fit in fit_passives_percentage: 
+            for effect in fit['effects']:
+                if effect.get('effect') == 'PASSIVE':
+                    stat = effect['stat']  
+                    formula = effect['formula']
+                    dv = effect['default_values']
+                    self.sum_dict(sum_fits_pasives_percentage, stat, self.calculate(character, formula, dv))
+        return sum_fits_passives_plain, sum_fits_pasives_percentage
 
     def sum_dict(self, dict, key, value):
         if key in dict:
@@ -86,6 +117,15 @@ class manager:
     def get_race(self, character): 
         res = self.connector.get_item(table_name='race', key={'id': character['race']})
         return res
+    
+    def get_fits(self, character): 
+        character_info_fits = list()
+        character_effect_fits = list()
+        for fit in character['info_fits']:
+            character_info_fits.append(self.connector.get_item(table_name='fit', key={'id': fit}))
+        for fit in character['effect_fits']:
+            character_effect_fits.append(self.connector.get_item(table_name='fit', key={'id': fit}))     
+        return character_info_fits, character_effect_fits
 
     def calculate_stats(self, all_stats_list, character): 
         all_stats_dict = dict()
@@ -113,14 +153,13 @@ class manager:
     def precalculate(self, character, formula, dv):
         for route in dv: 
             value = pydash.get(character, route)
-            print(f'{route}: {value}')
             if value is not None:
                 if type(value) == list:
                     dv[route] = sum(value)
                 else:
                     dv[route] = value
             formula = formula.replace(route, str(dv[route]))
-        return formula
+        return formula 
      
     def pre_calculate_skills(self, character, e): 
         element_skills_dict = list() 
